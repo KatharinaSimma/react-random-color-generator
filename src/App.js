6fcf3e26be09c78210808d044220693be701ab98
Katharina Simma <katharina.simma@diebin.at>
2023-01-16 15:44:50 +0100

    Add base functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import './App.css';
-import logo from './logo.svg';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
+const hues = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple',
+  'pink',
+  'monochrome',
+  'random',
+];
+
+const luminosities = ['bright', 'light', 'dark', 'random'];
 
 function App() {
+  const [hue, setHue] = useState('random');
+  const [luminosity, setLuminosity] = useState('random');
+  const [color, setColor] = useState(
+    randomColor({
+      luminosity: luminosity,
+      hue: hue,
+    }),
+  );
+  console.log(hue, luminosity);
+  console.log(color);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Random Color Generator</h1>
+      <p>This is my awesome Color Generator. Try it out!</p>
+      <h2>Choose your hue</h2>
+      {hues.map((h) => {
+        return (
+          <button key={h} onClick={() => setHue(h)}>
+            {h}
+          </button>
+        );
+      })}
+      <h2>Choose your luminosity</h2>
+      {luminosities.map((l) => {
+        return (
+          <button key={l} onClick={() => setLuminosity(l)}>
+            {l}
+          </button>
+        );
+      })}
+      <br />
+      <button
+        onClick={() => {
+          setColor(
+            randomColor({
+              luminosity: luminosity,
+              hue: hue,
+            }),
+          );
+        }}
+      >
+        Generate
+      </button>
+      <div
+        style={{
+          height: '100px',
+          width: '200px',
+          textAlign: 'center',
+          verticalAlign: 'center',
+          backgroundColor: color,
+        }}
+      >
+        <span style={{ color: color, filter: 'invert(100%)' }}>
+          Generated Color: {color}
+        </span>
+      </div>
+    </>
   );
 }
 
